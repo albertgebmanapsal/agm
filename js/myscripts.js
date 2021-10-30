0be73ee84d038a4cbc86565f66af1701aaf2d8fe
a_geb <93265115+albertgebmanapsal@users.noreply.github.com>
2021-10-31 01:25:13 +0800

    >Git push

---
+++ js/myscripts.js
@@ -0,0 +1,12 @@
+function myFunction() 
+  {
+   var x = document.getElementById("popupAboutme");
+    if (x.style.display === "block") 
+        {
+        x.style.display = "none";
+        } 
+    else 
+    {
+    x.style.display = "block";
+    }
+  }
